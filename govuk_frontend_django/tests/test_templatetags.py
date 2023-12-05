e2d2199d2c386434568b4fb828654739c714c549
Cameron Lamb <cameron.lamb@digital.trade.gov.uk>
2023-12-05 18:48:01 +0000

    Add Django 5.0 support and update with a blank test (#32)
    * Poetry updates
    
    * Update to test locally with tox
    
    * Add tox tests to CI
    
    * Separate dependencies into groups
    
    * Update makefile command
    
    * Remove deprecated warning
    
    * Update CI task
    
    * Add an empty test
    
    * Add a TODO

---
+++ govuk_frontend_django/tests/test_templatetags.py
@@ -0,0 +1,3 @@
+# TODO: Add some tests
+def test_empty():
+    assert True
--- settings.py
+++ settings.py
@@ -31,6 +31,5 @@ DATABASES = {
 LANGUAGE_CODE = "en-us"
 TIME_ZONE = "UTC"
 USE_I18N = True
-USE_L10N = True
 USE_TZ = True
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
